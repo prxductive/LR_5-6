670fda753c8047396fe6fd8ad30765e9cc8c9758
prxductive <144677265+prxductive@users.noreply.github.com>
2023-11-09 11:54:48 +0300

    Add files via upload

---
+++ Zadanie_4.pas
@@ -0,0 +1,37 @@
+﻿program z_4;
+const
+  Sz = 30; // размер массива
+
+var
+  i, x: integer;
+  a: array[1..Sz] of integer; 
+  b: array of integer; 
+
+begin
+  randomize; 
+  SetLength(b, 0); 
+
+  for i := 1 to Sz do 
+  begin
+    x := random(167) - 99; 
+    a[i] := x; 
+
+    if x mod 2 = 0 then
+    begin
+      SetLength(b, Length(b) + 1); 
+      b[High(b)] := x; 
+    end;
+  end;
+
+  for i := 1 to Sz do 
+    write(a[i], ' ');
+
+  writeln; 
+
+  for i := 0 to High(b) do 
+    write(b[i], ' ');
+end.
+
+  
+
+  
---
+++ Zadanie_5.pas
@@ -0,0 +1,37 @@
+﻿Program z_5;
+var
+  array1, array2: array[1..10] of integer; 
+  sum1, sum2 : integer;
+  i : integer;
+  begin
+    for i := 1 to 10 do 
+    begin
+      array1[i] := Random(21) - 10; 
+      array2[i] := Random(21) - 10;
+    end;
+      
+    sum1 := 0;
+    sum2 := 0;
+    
+    for i := 1 to 10 do 
+    begin
+      sum1 := sum1 + max(0, array1[i]); 
+      sum2 := sum2 + max(0, array2[i]);
+    end;
+    if sum1 < sum2 then 
+      for i := 1 to 10 do 
+        array1[i] := array1[i] * 10 
+      else
+        for i := 1 to 10 do
+          array2[i] := array2[i] * 10; 
+        
+        writeln('Первый массив:'); 
+  for i := 1 to 10 do 
+    write(array1[i], ' '); 
+  writeln; 
+  
+  writeln('Второй массив:'); 
+  for i := 1 to 10 do 
+    write(array2[i], ' '); 
+  writeln;      
+  end.
